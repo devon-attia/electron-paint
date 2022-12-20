18577ce38e32d4a372d68ac3bd1e7325a4de5c17
Devon Attia <devon.attia@mzero.com>
2022-12-20 14:20:01 -0500

    added paperJS and some contextbridge scaffolding

--- main.ts
+++ main.ts
@@ -1,9 +1,13 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.ts'),
+    },
   });
 
   win.webContents.openDevTools();
@@ -13,7 +17,6 @@ const createWindow = () => {
 app.whenReady().then(() => {
   createWindow();
 
-  let test = 1;
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
