d54fb70b001f68dbc40b5788bfd4e9d46153f579
Devon Attia <devon.attia@mzero.com>
2022-12-20 14:39:02 -0500

    basic free-hand drawing tool

--- canvas.js
+++ canvas.js
@@ -1,16 +1,18 @@
+paper.install(window);
 window.onload = function() {
     let canvas = document.querySelector('canvas');
     paper.setup(canvas);
-    // Create a Paper.js Path to draw a line into it:
-    var path = new paper.Path();
-    // Give the stroke a color
-    path.strokeColor = 'black';
-    var start = new paper.Point(100, 100);
-    // Move to start and draw a line from there
-    path.moveTo(start);
-    // Note that the plus operator on Point objects does not work
-    // in JavaScript. Instead, we need to call the add() function:
-    path.lineTo(start.add([ 200, -50 ]));
-    // Draw the view now:
-    paper.view.draw();
+    
+    let tool = new Tool();
+    let path;
+
+    tool.onMouseDown = function(event) {
+        path = new Path();
+        path.strokeColor = 'white';
+        path.add(event.point);
+    }
+
+    tool.onMouseDrag = function(event) {
+        path.add(event.point);
+    }
 }
